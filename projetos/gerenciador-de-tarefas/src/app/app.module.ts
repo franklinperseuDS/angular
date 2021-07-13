b8c9c94cb30c0102b00fd0f8c2b925506387fa0b
Franklin Perseu de Lima e Lima <69854802+franklinperseuDS@users.noreply.github.com>
2021-07-13 16:44:52 -0400

    criando as funções no service

--- projetos/gerenciador-de-tarefas/src/app/app.module.ts
+++ projetos/gerenciador-de-tarefas/src/app/app.module.ts
@@ -1,9 +1,11 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
+import {TarefasModule} from './tarefas';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    TarefasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
