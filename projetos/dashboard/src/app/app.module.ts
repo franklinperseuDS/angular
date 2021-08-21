b395ce806afd531079e627f51905cd735848c044
Franklin Perseu de Lima e Lima <69854802+franklinperseuDS@users.noreply.github.com>
2021-08-21 17:25:42 -0400

    consumindo api
    consumindo api do google e gerando 6 gráficos

---
+++ projetos/dashboard/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DashboardModule,
+    FormsModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
